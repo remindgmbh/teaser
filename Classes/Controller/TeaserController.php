400121dbfb76f7f9ef809ea482b8c2266f08364f
Dominik Lehrl <d.lehrl@remind.de>
2025-02-28 12:56:31 +0100

    fix: process typolink url

--- Classes/Controller/TeaserController.php
+++ Classes/Controller/TeaserController.php
@@ -7,15 +7,24 @@ namespace Remind\Teaser\Controller;
 use JsonSerializable;
 use Psr\Http\Message\ResponseInterface;
 use Remind\Teaser\Domain\Repository\TeaserRepository;
+use TYPO3\CMS\Core\LinkHandling\TypoLinkCodecService;
 use TYPO3\CMS\Core\Service\FlexFormService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 class TeaserController extends ActionController
 {
+    protected TypoLinkCodecService $typoLinkCodecService;
+
+    protected ContentObjectRenderer $contentObjectRenderer;
+
     public function __construct(
         private readonly TeaserRepository $teaserRepository,
         private readonly FlexFormService $flexFormService,
     ) {
+        $this->typoLinkCodecService = GeneralUtility::makeInstance(TypoLinkCodecService::class);
+        $this->contentObjectRenderer = GeneralUtility::makeInstance(ContentObjectRenderer::class);
     }
 
     public function selectionListAction(): ResponseInterface
@@ -44,6 +53,8 @@ class TeaserController extends ActionController
                 $record['categories'] = $this->serializeCategories($categories);
             }
 
+            $record['link'] = $this->processTypolinkUrl($record['link']);
+
             return $record;
         }, $recordUids);
 
@@ -66,5 +77,17 @@ class TeaserController extends ActionController
                 'uid' => $category->getUid(),
             ];
         }, $categories);
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    protected function processTypolinkUrl(string $link): array
+    {
+        $typoLinkConfiguration = $this->typoLinkCodecService->decode($link);
+        $typoLinkConfiguration['href'] =
+            $this->contentObjectRenderer->createUrl(['parameter' => $typoLinkConfiguration['url']]);
+
+        return $typoLinkConfiguration;
     }
 }
